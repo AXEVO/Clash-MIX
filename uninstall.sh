57b0c4595565ed6efb5d3db8c1e405bdbea700fd
AXEVO <115725008+AXEVO@users.noreply.github.com>
2023-11-18 23:51:31 +0800

    卸载时删除旧的备份

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,7 @@ Clash_delete_dir="/data/clash.delete"
 rm_data() {
     mv ${Clash_data_dir} ${Clash_delete_dir}
     rm -rf ${Clash_data_dir}
+    rm -rf /data/clash.old
     rm -rf /data/adb/service.d/clash_service.sh
     #rm -rf /data/data/xyz.chz.clash/
     #rm -rf /data/app/xyz.chz.clash/
