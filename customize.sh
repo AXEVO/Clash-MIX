6cf18490d2c736da9b564369ac9c72568c6d7cc2
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-14 00:33:27 +0800

    初步上传

---
+++ customize.sh
@@ -0,0 +1,16 @@
+SKIPUNZIP=1
+
+# 定义安装路径
+Module_dir=/data/adb/modules/Clash
+
+# 解压 ZIP 文件到模块路径
+unzip -o "${ZIPFILE}" -x 'META-INF/*' -d $MODPATH >&2
+
+# 删除已存在的安装目录并创建新目录
+rm -rf ${Module_dir} && mkdir -p ${Module_dir}
+
+# 移动解压的 Clash 文件夹到安装目录
+mv ${MODPATH}/Clash/* ${Module_dir}/
+
+# 设置安装目录的权限
+chmod 755 -Rf ${Module_dir}
