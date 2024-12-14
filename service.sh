fccbeb08507c158157d18508b99fd52a86b585fa
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-14 15:00:52 +0800

    初步修改一些明显问题

--- service.sh
+++ service.sh
@@ -1,11 +1,11 @@
 # 等待设备启动完成
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
-    sleep 5
+    sleep 2
 done
 
 # 启动 Clash 服务
-/data/adb/modules/Clash/Script/Clash.Service start
+/data/adb/modules/Clash/Scripts/Clash.Service start
 
 # 监听 disable 文件的创建和删除事件
-inotifyd /data/adb/modules/Clash/Script/Clash.Inotify "/data/adb/modules/Clash" >> /dev/null &
+inotifyd /data/adb/modules/Clash/Scripts/Clash.Inotify "/data/adb/modules/Clash" >> /dev/null &
 
