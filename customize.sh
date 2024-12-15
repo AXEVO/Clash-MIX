4de28e39d111dff005491edd5751a82fd16ff780
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-15 12:31:27 +0800

    更新安装脚本和部分路径

--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
 SKIPUNZIP=1
 
-rm -rf $MODPATH && mkdir -p $MODPATH
-unzip -o "${ZIPFILE}" -x 'META-INF/*' 'customize.sh' -d $MODPATH >&2
-mv $MODPATH/Clash/* $MODPATH && rm -rf $MODPATH/Clash/
+rm -rf $MODPATH/*
+unzip -o $ZIPFILE /Clash -d $TMPDIR >&2
+mv $TMPDIR/Clash/* $MODPATH
 chmod 755 -Rf $MODPATH
