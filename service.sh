058423ddbd7eb111f590372c7fa6086f06c3dda1
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-15 23:33:25 +0800

    确认能用的模块，先提交一次

--- service.sh
+++ service.sh
@@ -1,11 +1,8 @@
-# 等待设备启动完成
 until [ "$(getprop sys.boot_completed)" = "1" ]; do
     sleep 2
 done
 script_path=/data/adb/modules/Clash/Scripts
-# 启动 Clash 服务
-$script_path/Clash.Service start
 
-# 监听 disable 文件的创建和删除事件
+$script_path/Clash.Service start
 inotifyd $script_path/Clash.Inotify "/data/adb/modules/Clash" >> /dev/null &
 
