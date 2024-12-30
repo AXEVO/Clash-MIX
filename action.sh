db7857ece330b2fb122efa4443349d28cc9e4756
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-30 21:40:58 +0800

    尝试一下下

---
+++ action.sh
@@ -0,0 +1,5 @@
+script_path=/data/adb/modules/Clash/Scripts
+
+$script_path/Clash.Service stop
+sleep 3
+$script_path/Clash.Service start
--- customize.sh
+++ customize.sh
@@ -1,4 +1,14 @@
 SKIPUNZIP=1
+sdcard_dir="/data/media/0/Android"
 
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
-chmod 755 -Rf $MODPATH
+
+mv "$MODPATH/Clash" "$sdcard_dir"
+ln -sf "$sdcard_dir/Clash/Clash配置文件.yaml" "$MODPATH/Proxy/config.yaml"
+
+if [ "$KSU" = true ]; then
+  ln -sf $MODPATH/Proxy/WebUI $MODPATH/webroot
+fi
+
+
+chmod 777 -Rf $MODPATH
