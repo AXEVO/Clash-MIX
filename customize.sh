e5bc7151563c8ceeb06769aade81ef9a3d9543e5
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-31 23:33:19 +0800

    使用cp命令解决mv存在的各种报错

--- customize.sh
+++ customize.sh
@@ -5,9 +5,10 @@ unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
 mkdir -p $sdcard_dir/Clash
 if [ -f "$sdcard_dir/Clash/Clash配置.yaml" ]; then
-  mv "$sdcard_dir/Clash/Clash配置.yaml" "$sdcard_dir/Clash/更新模块前的Clash配置.yaml"
+  cp "$sdcard_dir/Clash/Clash配置.yaml" "$sdcard_dir/Clash/更新模块前的Clash配置.yaml"
+  rm -rf $sdcard_dir/Clash/Clash配置.yaml
 fi
-mv -f $MODPATH/Clash/* "$sdcard_dir/Clash/"
+cp -rf $MODPATH/Clash/* "$sdcard_dir/Clash/"
 rm -rf $MODPATH/Clash
 
 ln -sf "$sdcard_dir/Clash/Clash配置.yaml" "$MODPATH/Proxy/config.yaml"
