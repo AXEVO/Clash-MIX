84a48ea24f1351e5a4e6426cd0da3325730b0bc1
ascvuoe <115725008+AXEVO@users.noreply.github.com>
2024-12-31 23:10:11 +0800

    修复路径和安装脚本

--- customize.sh
+++ customize.sh
@@ -1,14 +1,19 @@
 SKIPUNZIP=1
-sdcard_dir="/data/media/0/Android"
+sdcard_dir="/mnt/user/0/emulated/0/Android"
 
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
 
-mv "$MODPATH/Clash" "$sdcard_dir"
-ln -sf "$sdcard_dir/Clash/Clash配置文件.yaml" "$MODPATH/Proxy/config.yaml"
+mkdir -p $sdcard_dir/Clash
+if [ -f "$sdcard_dir/Clash/Clash配置.yaml" ]; then
+  mv "$sdcard_dir/Clash/Clash配置.yaml" "$sdcard_dir/Clash/更新模块前的Clash配置.yaml"
+fi
+mv -f $MODPATH/Clash/* "$sdcard_dir/Clash/"
+rm -rf $MODPATH/Clash
+
+ln -sf "$sdcard_dir/Clash/Clash配置.yaml" "$MODPATH/Proxy/config.yaml"
 
 if [ "$KSU" = true ]; then
-  ln -sf $MODPATH/Proxy/WebUI $MODPATH/webroot
+  ln -sf /data/adb/modules/Clash/Proxy/WebUI $MODPATH/webroot
 fi
-
 
 chmod 777 -Rf $MODPATH
