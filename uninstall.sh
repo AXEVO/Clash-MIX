c07ae793dc9e5de5d715a58fe713fd4501701add
AXEVO <115725008+AXEVO@users.noreply.github.com>
2025-01-01 17:05:36 +0800

    添加卸载脚本并修改换行符避免可能的问题

---
+++ uninstall.sh
@@ -0,0 +1,15 @@
+until [ "$(getprop sys.boot_completed)" = "1" ]; do
+    sleep 2
+done
+
+until [ -d "/sdcard/Android" ]; do
+    sleep 2
+done
+
+sdcard_dir="/sdcard"
+
+if [ -f "$sdcard_dir/Android/Clash/Clash配置.yaml" ]; then
+  cp "$sdcard_dir/Android/Clash/Clash配置.yaml" "$sdcard_dir/Android/Clash模块配置-卸载备份.yaml"
+fi
+
+rm -rf $sdcard_dir/Android/Clash
