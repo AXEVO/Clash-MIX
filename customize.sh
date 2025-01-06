9f9b44f4d6f6c148d1d6412ca4ac43be16e519c7
AXEVO <115725008+AXEVO@users.noreply.github.com>
2025-01-06 23:41:03 +0800

    更改配置以便彻底修好IPv6，解决ksu卸载模块卡开机以及其他小修小补

--- customize.sh
+++ customize.sh
@@ -1,8 +1,10 @@
 SKIPUNZIP=1
+module_dir="/data/adb/modules/Clash"
 sdcard_dir="/sdcard/Android"
 
+#解压全部文件到模块安装目录
 unzip -o "$ZIPFILE" -x 'META-INF/*' -d $MODPATH >&2
-
+#开始处理内部存储中的配置
 mkdir -p $sdcard_dir/Clash
 if [ -f "$sdcard_dir/Clash/Clash配置.yaml" ]; then
   cp "$sdcard_dir/Clash/Clash配置.yaml" "$sdcard_dir/Clash/更新模块前的Clash配置.yaml"
@@ -10,11 +12,18 @@ if [ -f "$sdcard_dir/Clash/Clash配置.yaml" ]; then
 fi
 cp -rf $MODPATH/Clash/* "$sdcard_dir/Clash/"
 rm -rf $MODPATH/Clash
-
+#不覆盖用户自定义规则
+if [ -d "$module_dir/Proxy/rule_providers" ]; then
+  rm -rf $MODPATH/Proxy/rule_providers/userDirect.yaml
+  rm -rf $MODPATH/Proxy/rule_providers/userProxy.yaml
+  cp "$module_dir/Proxy/rule_providers/userDirect.yaml" "$MODPATH/Proxy/rule_providers/userDirect.yaml"
+  cp "$module_dir/Proxy/rule_providers/userProxy.yaml" "$MODPATH/Proxy/rule_providers/userProxy.yaml"
+fi
+#配置软链接
 ln -sf "$sdcard_dir/Clash/Clash配置.yaml" "$MODPATH/Proxy/config.yaml"
-
+#适配KSU
 if [ "$KSU" = true ]; then
   ln -sf /data/adb/modules/Clash/Proxy/WebUI $MODPATH/webroot
 fi
-
+#---
 chmod 777 -Rf $MODPATH
--- uninstall.sh
+++ uninstall.sh
@@ -1,15 +1,17 @@
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
-    sleep 2
-done
+{
+  until [ "$(getprop sys.boot_completed)" = "1" ]; do
+      sleep 2
+  done
 
-until [ -d "/sdcard/Android" ]; do
-    sleep 2
-done
+  until [ -d "/sdcard/Android" ]; do
+      sleep 2
+  done
 
-sdcard_dir="/sdcard"
+  sdcard_dir="/sdcard"
 
-if [ -f "$sdcard_dir/Android/Clash/Clash配置.yaml" ]; then
-  cp "$sdcard_dir/Android/Clash/Clash配置.yaml" "$sdcard_dir/Android/Clash模块配置-卸载备份.yaml"
-fi
+  if [ -f "$sdcard_dir/Android/Clash/Clash配置.yaml" ]; then
+    cp "$sdcard_dir/Android/Clash/Clash配置.yaml" "$sdcard_dir/Android/Clash模块配置-卸载备份.yaml"
+  fi
 
-rm -rf $sdcard_dir/Android/Clash
+  rm -rf "$sdcard_dir/Android/Clash"
+} &
