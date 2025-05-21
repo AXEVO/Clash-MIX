7c039fe8841d4dce9b0a466925094829b73538cf
Keeperorowner <1832713361@qq.com>
2025-05-22 00:59:56 +0800

    Update service.sh
    Start the kernel and create a tag file at the same time

--- service.sh
+++ service.sh
@@ -1,12 +1,14 @@
-until [ "$(getprop sys.boot_completed)" = "1" ]; do
+until ["$(getprop sys.boot_completed)" = "1" ]; do
     sleep 2
 done
 
-until [ -d "/sdcard/Android" ]; do
+until [-d "/sdcard/Android"]; do
     sleep 2
 done
 
 script_path=/data/adb/modules/Clash/Scripts
+status_file=/data/adb/modules/Clash/clash_status
 
 $script_path/Clash.Service start
 inotifyd $script_path/Clash.Inotify "/data/adb/modules/Clash" >> /dev/null &
+touch "$status_file"
