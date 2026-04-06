7cfcf1cead613af00f68c1e69abd378e950c73a6
AXEVO <115725008+AXEVO@users.noreply.github.com>
2026-04-06 17:15:05 +0800

    修复卸载备份的：导致的异常

--- uninstall.sh
+++ uninstall.sh
@@ -13,7 +13,7 @@
 
   if [ -d "$clash_dir" ]; then
 
-    backup_dir="$sdcard_dir/Android/Clash卸载备份-[$(date +%Y.%m.%d-%H:%M)]"
+    backup_dir="$sdcard_dir/Android/Clash卸载备份-[$(date +%Y.%m.%d-%H.%M)]"
 
     mkdir -p "$backup_dir"
 
@@ -23,9 +23,9 @@
 
     [ -f "$clash_dir/工具/自定义代理.yaml" ] && cp "$clash_dir/工具/自定义代理.yaml" "$backup_dir/自定义代理.yaml"
 
-    echo "此目录为Clash MIX卸载后配置备份，欢迎下次使用" > "$backup_dir/说明.txt"
-    echo "Telegram频道@wocao_esu：  https://t.me/wocao_esu" >> "$backup_dir/说明.txt"
-    echo "Github： https://github.com/AXEVO/Clash-MIX" >> "$backup_dir/说明.txt"
+    echo "此目录为 Clash MIX 卸载后配置备份，欢迎下次使用" > "$backup_dir/说明.txt"
+    echo "Telegram频道  @wocao_esu  https://t.me/wocao_esu" >> "$backup_dir/说明.txt"
+    echo "Github  https://github.com/AXEVO/Clash-MIX" >> "$backup_dir/说明.txt"
 
     rm -rf "$clash_dir"
 
