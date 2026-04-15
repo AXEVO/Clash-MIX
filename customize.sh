e93a8086757dfebf278ffeba4b9823fa3883108e
AXEVO <115725008+AXEVO@users.noreply.github.com>
2026-04-15 22:26:04 +0800

    修复安装提示显示效果

--- customize.sh
+++ customize.sh
@@ -40,41 +40,41 @@ case "$install_mode" in
     cp -af "$sdcard_work/工具/自定义直连.yaml" "$MODPATH/Clash/工具/自定义直连.yaml" 2>/dev/null
 
     if should_keep_old_config "$sdcard_work/Clash配置.yaml" "$MODPATH/Clash/Clash配置.yaml"; then
-      ui_print "🟩配置文件无更新 保留原有 Clash配置.yaml🟩"
-      ui_print "🟩模块内置Clash配置文件模板备份位于🟩"
-      ui_print "🟩[内部存储/Android/Clash/资料/配置模板]🟩"
+      ui_print "🟩配置文件无更新 保留原有 Clash配置.yaml"
+      ui_print "🟩模块内置Clash配置文件模板备份位于"
+      ui_print "🟩[内部存储/Android/Clash/资料/配置模板]"
       cp -af "$sdcard_work/Clash配置.yaml" "$MODPATH/Clash/Clash配置.yaml"
     else
       cp -af "$sdcard_work/Clash配置.yaml" "$MODPATH/Clash/更新前配置.yaml"
-      ui_print "🟨Clash配置.yaml已更新 需要重新填写订阅链接🟨"
-      ui_print "🟨安装模块更新前的Clash配置文件已备份到🟨"
-      ui_print "🟨[内部存储/Android/Clash/更新前配置.yaml]🟨"
-      ui_print "🟨请从旧配置中复制订阅链接后 再填写到新配置🟨"
+      ui_print "🟨Clash配置.yaml已更新 需要重新填写订阅链接"
+      ui_print "🟨安装模块更新前的Clash配置文件已备份到"
+      ui_print "🟨[内部存储/Android/Clash/更新前配置.yaml]"
+      ui_print "🟨请从旧配置中复制订阅链接后 再填写到新配置"
     fi
     ;;
 
   restore)
-    ui_print "🟨检测到$(basename "$backup_dir")🟨"
+    ui_print "🟨检测到$(basename "$backup_dir")"
 
     cp -af "$backup_dir/自定义代理.yaml" "$MODPATH/Clash/工具/自定义代理.yaml" 2>/dev/null
     cp -af "$backup_dir/自定义直连.yaml" "$MODPATH/Clash/工具/自定义直连.yaml" 2>/dev/null
 
     if should_keep_old_config "$backup_dir/Clash配置.yaml" "$MODPATH/Clash/Clash配置.yaml"; then
-      ui_print "🟩配置文件无更新 使用卸载备份的配置.yaml🟩"
-      ui_print "🟩模块内置Clash配置文件模板备份位于🟩"
-      ui_print "🟩[内部存储/Android/Clash/资料/配置模板]🟩"
+      ui_print "🟩配置文件无更新 使用卸载备份的配置.yaml"
+      ui_print "🟩模块内置Clash配置文件模板备份位于"
+      ui_print "🟩[内部存储/Android/Clash/资料/配置模板]"
       cp -af "$backup_dir/Clash配置.yaml" "$MODPATH/Clash/Clash配置.yaml"
     else
       cp -af "$backup_dir/Clash配置.yaml" "$MODPATH/Clash/卸载备份配置.yaml"
-      ui_print "🟨Clash配置.yaml已更新 卸载备份已复制到🟨"
-      ui_print "🟨[内部存储/Android/Clash/卸载备份配置]🟨"
-      ui_print "🟨请从旧配置中复制订阅链接后 再填写到新配置🟨"
+      ui_print "🟨Clash配置.yaml已更新 卸载备份已复制到"
+      ui_print "🟨[内部存储/Android/Clash/卸载备份配置]"
+      ui_print "🟨请从旧配置中复制订阅链接后 再填写到新配置"
     fi
     ;;
 
   fresh)
-    ui_print "🟨全新安装 请安装完成后填写订阅链接并重启🟨"
-    ui_print "🟨[内部存储/Android/Clash/Clash配置.yaml]🟨"
+    ui_print "🟨全新安装 请安装完成后填写订阅链接并重启"
+    ui_print "🟨[内部存储/Android/Clash/Clash配置.yaml]"
     ;;
 esac
 
